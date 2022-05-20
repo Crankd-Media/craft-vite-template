a67b2401164ddccfb993247959151f3d9dbe60c3
braedencrankd <braeden@crankd.nz>
2022-05-20 13:20:52 +1200

    Added documentation pages to site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./templates/**/*.{twig,html,js,ts}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#0070f3",
+          dark: "#0070f3",
+          light: "#0070f3",
+        },
+        secondary: {
+          DEFAULT: "#ffc107",
+          dark: "#ffc107",
+          light: "#ffc107",
+        },
+        success: {
+          DEFAULT: "#28a745",
+          dark: "#28a745",
+          light: "#28a745",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
